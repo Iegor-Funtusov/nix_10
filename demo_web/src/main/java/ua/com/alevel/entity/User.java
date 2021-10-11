c937db98be8671442c024ffd69339bfc43fae87e
Iegor-Funtusov <funtushan@gmail.com>
2021-10-11 18:35:57 +0300

    add ssr and rest

---
+++ demo_web/src/main/java/ua/com/alevel/entity/User.java
@@ -0,0 +1,38 @@
+package ua.com.alevel.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long age;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
