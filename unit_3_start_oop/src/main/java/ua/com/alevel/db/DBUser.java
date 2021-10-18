ee13124ab5e4c9eb4460a15bd5cd01d88d7dc18f
Iegor-Funtusov <funtushan@gmail.com>
2021-10-18 18:07:53 +0300

    add crud process

---
+++ unit_3_start_oop/src/main/java/ua/com/alevel/db/DBUser.java
@@ -0,0 +1,58 @@
+package ua.com.alevel.db;
+
+import ua.com.alevel.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class DBUser {
+
+    private final List<User> users;
+    private static DBUser instance;
+
+    private DBUser() {
+        users = new ArrayList<>();
+    }
+
+    public static DBUser getInstance() {
+        if (instance == null) {
+            instance = new DBUser();
+        }
+        return instance;
+    }
+
+    public void create(User user) {
+        user.setId(generateId());
+        users.add(user);
+    }
+
+    public void update(User user) {
+        User current = findById(user.getId());
+        current.setName(user.getName());
+        current.setAge(user.getAge());
+    }
+
+    public void delete(String id) {
+        users.removeIf(user -> user.getId().equals(id));
+    }
+
+    public User findById(String id) {
+        return users.stream()
+                .filter(u -> u.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("user not found"));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    private String generateId() {
+        String id = UUID.randomUUID().toString();
+        if (users.stream().anyMatch(user -> user.getId().equals(id))) {
+            return generateId();
+        }
+        return id;
+    }
+}
