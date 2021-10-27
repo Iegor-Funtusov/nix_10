995421b17819be5a07dd4980e6f3db6086829a48
Iegor-Funtusov <funtushan@gmail.com>
2021-10-27 15:40:38 +0300

    add logs

--- unit_5_test_logs/src/main/java/ua/com/alevel/service/UserService.java
+++ unit_5_test_logs/src/main/java/ua/com/alevel/service/UserService.java
@@ -1,19 +1,29 @@
 package ua.com.alevel.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ua.com.alevel.dao.UserDao;
 import ua.com.alevel.entity.User;
 
 import java.util.List;
 
 public class UserService {
-    
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+    private static final Logger LOGGER_INFO = LoggerFactory.getLogger("info");
+    private static final Logger LOGGER_WARN = LoggerFactory.getLogger("warn");
+    private static final Logger LOGGER_ERROR = LoggerFactory.getLogger("error");
+
     private final UserDao userDao = new UserDao();
 
     public void create(User user) {
+        LOGGER_INFO.info("start create user");
         if (!userDao.existByEmail(user.getEmail())) {
             userDao.create(user);
+            LOGGER_INFO.info("finish create user");
         } else {
             System.out.println("user exist by email");
+            LOGGER_WARN.warn("user exist by email: " + user.getEmail());
         }
     }
 
