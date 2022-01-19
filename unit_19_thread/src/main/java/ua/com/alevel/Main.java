c4b0f7031495bcd8e26ea374be14e87290f46931
egorfuntusov <funtushan@gmail.com>
2022-01-19 16:23:25 +0200

    parallel future

--- unit_19_thread/src/main/java/ua/com/alevel/Main.java
+++ unit_19_thread/src/main/java/ua/com/alevel/Main.java
@@ -4,17 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
         // Get current size of heap in bytes
-        long heapSize = Runtime.getRuntime().totalMemory();
-        System.out.println("heapSize = " + heapSize);
+//        long heapSize = Runtime.getRuntime().totalMemory();
+//        System.out.println("heapSize = " + heapSize);
+//
+//// Get maximum size of heap in bytes. The heap cannot grow beyond this size.// Any attempt will result in an OutOfMemoryException.
+//        long heapMaxSize = Runtime.getRuntime().maxMemory();
+//        System.out.println("heapMaxSize = " + heapMaxSize);
+//
+//        // Get amount of free memory within the heap in bytes. This size will increase // after garbage collection and decrease as new objects are created.
+//        long heapFreeSize = Runtime.getRuntime().freeMemory();
+//        System.out.println("heapFreeSize = " + heapFreeSize / 1024 );
 
-// Get maximum size of heap in bytes. The heap cannot grow beyond this size.// Any attempt will result in an OutOfMemoryException.
-        long heapMaxSize = Runtime.getRuntime().maxMemory();
-        System.out.println("heapMaxSize = " + heapMaxSize);
+//        ProgramRun.run();
+        System.out.println("start");
+        SomeThread someThread = new SomeThread();
+        someThread.start();
+        someThread.interrupt();
+        System.out.println("finish");
+    }
 
-        // Get amount of free memory within the heap in bytes. This size will increase // after garbage collection and decrease as new objects are created.
-        long heapFreeSize = Runtime.getRuntime().freeMemory();
-        System.out.println("heapFreeSize = " + heapFreeSize / 1024 );
+    private static class SomeThread extends Thread {
 
-        ProgramRun.run();
+        @Override
+        public void run() {
+            System.out.println("Hello world");
+        }
     }
 }
